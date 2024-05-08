71adb4996f6e1aaba7ea9987ea97611620a66d28
Joshua Maldonado <joshuamaldonado66gmail.com>
2024-05-07 20:18:18 -0400

    creating new sentences

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[str.find("object-oriented"):].split()[0] + " programming with Python")
+print(" ".join(str.split()[5:8]) + " with Python\n")

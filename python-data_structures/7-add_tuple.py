f5e83cbd601f495747ce277b48e9e0b1c2202273
Joshua Maldonado <joshuamaldonado66gmail.com>
2024-05-13 17:42:21 -0400

    task 7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
-
-
 def add_tuple(tuple_a=(), tuple_b=()):
+    a = (0, 0)
+    b = (0, 0)
 
     if len(tuple_a) == 0:
-        a, b = (0, 0)
+        a = (0, 0)
     elif len(tuple_a) == 1:
-        a = tuple_a[0]
-        b = 0
-    else:
-        a = tuple_a[0]
-        b = tuple_a[1]
-    if len(tuple_b) == 0:
-        c, d = (0, 0)
-    elif len(tuple_b) == 1:
-        c = tuple_a[0]
-        d = 0
-    else:
-        c = tuple_b[0]
-        d = tuple_b[1]
-    new_tuple = (a + c, b + d)
+        a = (tuple_a[0], 0)
+    elif len(tuple_a) >= 2:
+        a = (tuple_a[0], tuple_a[1])
 
-    return(new_tuple)
+    if len(tuple_b) == 0:
+        b = (0, 0)
+    elif len(tuple_b) == 1:
+        b = (tuple_b[0], 0)
+    elif len(tuple_b) >= 2:
+        b = (tuple_b[0], tuple_b[1])
+
+    return (a[0] + b[0], a[1] + b[1])

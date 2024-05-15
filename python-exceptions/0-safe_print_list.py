bf827689dda9f58d5a06dabbc19e121506e545a8
Joshua Maldonado <joshuamaldonado66gmail.com>
2024-05-15 16:12:10 -0400

    task 0 and readme

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -7,5 +7,6 @@ def safe_print_list(my_list=[], x=0):
             count = count + 1
     except IndexError:
         pass
-    print() # Print newline after all elements
+    print()  # Print newline after all elements
+
     return count

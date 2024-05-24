0cf508b424eca11739cacb82708f381989d9485c
Joshua Maldonado <joshuamaldonado66gmail.com>
2024-05-24 15:08:50 -0400

    task 01

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -8,6 +8,8 @@ from math import pi
 class Circle:
 
     def __init__(self, radius):
+        if radius < 0:
+            raise ValueError("Radius cannot be negative")
         self.radius = radius
 
 

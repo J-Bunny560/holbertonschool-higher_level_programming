090025d43678110a37fa927eb1fcfb443acf6ea6
Joshua Maldonado <joshuamaldonado66gmail.com>
2024-05-24 19:23:43 -0400

    task 03

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -1,63 +1,19 @@
 #!/usr/bin/python3
+
 class CountedIterator:
-
     def __init__(self, iterable):
+        self.iterable = iter(iterable)
+        self.count = 0
 
-        self.iterator = iter(iterable)
-
-        self.counter = 0
-
-
-    def get_count(self):
-
-        return self.counter
-
+    def __iter__(self):
+        return self
 
     def __next__(self):
+        self.count += 1
+        return next(self.iterable)
 
-        self.counter += 1
+    def get_count(self):
+        return self.count
 
-        try:
-
-            return next(self.iterator)
-
-        except StopIteration:
-
-            raise StopIteration
-
-def test_counted_iterator():
-
-    counted_iter = CountedIterator([1, 2, 3, 4, 5])
-
-
-    for i in range(3):
-
-        next(counted_iter)
-
-
-    assert counted_iter.get_count() == 3, "Count should match the number of items"
-
-
-    iterator = iter(counted_iter)
-
-
-    while True:
-
-        try:
-
-            next(iterator)  # This should raise StopIteration
-
-        except StopIteration:
-
-            break
-
-
-    assert counted_iter.get_count() == 5, "Count should match the number of items"
-
-
-# Testing
-
-if __name__ == "__main__":
-
-    test_counted_iterator()
-
+data = [1, 2, 3, 4]
+counted_iter = CountedIterator(data)

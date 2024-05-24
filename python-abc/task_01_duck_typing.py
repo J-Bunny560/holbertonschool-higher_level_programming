2cb5fdf71ca46b8af67d9dc6d63a6498f409f319
Joshua Maldonado <joshuamaldonado66gmail.com>
2024-05-24 15:02:09 -0400

    task 01

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+import abc
+from math import pi
+
+
+#  Circle and Rectangle Classes
+
+class Circle:
+
+    def __init__(self, radius):
+        self.radius = radius
+
+
+    def area(self):
+        return pi * (self.radius ** 2)
+
+
+    def perimeter(self):
+        return 2 * pi * self.radius
+
+
+class Rectangle:
+
+    def __init__(self, width, height):
+
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return (self.width * self.height)
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+
+#  shape_info Function
+
+def shape_info(shape):
+
+    print(f"Area: {shape.area()}")
+    print(f"Perimeter: {shape.perimeter()}")

4d5346858d5b7b39dd0c59c2f7b5e5a1184e14d3
Joshua Maldonado <joshuamaldonado66gmail.com>
2024-06-01 22:10:55 -0400

    task 03

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -36,9 +36,9 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             self.wfile.write(json.dumps(response).encode('utf-8'))
         else:  # Handle all other paths as undefined
             self.send_response(404)
-            self.send_header('Content-type', 'application/json')
+            self.send_header('Content-type', 'text/plain')  # Set to text/plain for 404
             self.end_headers()
-            self.wfile.write(json.dumps({"error": "Endpoint not found"}).encode('utf-8'))
+            self.wfile.write(b"Endpoint not found")  # Send plain text error
 
 PORT = 8000
 

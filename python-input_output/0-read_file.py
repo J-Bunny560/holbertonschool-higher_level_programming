2ce7b1ddd17c56c97b55e75c9d96432316613c1c
J-Bunny560 <156542388+J-Bunny560@users.noreply.github.com>
2024-09-04 11:55:38 -0400

    Delete python-input_output directory

--- python-input_output/0-read_file.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""defines functions to read a txt file and prints to stdout"""
-
-
-def read_file(filename=""):
-    """reads text file and prints to stdout"""
-    with open(filename) as f:
-        print(f.read(), end="")
--- python-input_output/3-to_json_string.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""defines function to return JSON representation of an object"""
-
-
-def to_json_string(my_obj):
-    """return JSON representation of an object"""
-    import json
-    return json.dumps(my_obj, ensure_ascii=False)

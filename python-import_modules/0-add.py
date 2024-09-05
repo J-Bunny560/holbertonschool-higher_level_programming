8a40536ee9d979df0ebfce828b311abad5d5eb49
J-Bunny560 <joshuamaldonado66@gmail.com>
2024-09-05 18:01:31 -0400

    fix

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -4,5 +4,4 @@ import add_0
 if __name__ == "__main__":
     a = 1
     b = 2
-
-print("{:d} + {:d} = {:d}".format(a, b, add_0.add(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add_0.add(a, b)))

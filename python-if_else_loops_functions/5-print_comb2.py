5f4248beef695e080e7fddfc6600b1aa66081ce6
J-Bunny560 <joshuamaldonado66@gmail.com>
2024-09-05 15:52:00 -0400

    fix

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-    print(f"{i:02d}", end=", " if i < 99 else "\n")
+# print numbers from 0 to 99
+
+for i in range(0, 100):
+    if i < 99:
+        print("{:02d}".format(i), end=", ")
+    else:
+        print("{:02d}".format(i))

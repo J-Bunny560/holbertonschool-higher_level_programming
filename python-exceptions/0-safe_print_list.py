7455647b0ffac6ca5c6d7262798ee060e79e2108
J-Bunny560 <joshuamaldonado66@gmail.com>
2024-09-11 11:02:31 -0400

    fix

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,12 +1,10 @@
 #!/user/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        printed = 0
         for i in range(x):
             print(my_list[i], end='')
-            printed += 1
         print()
-        return printed
+        return min(x, len(my_list))
     except:
         print()
         return 0

ba4880bf2a7264832f780f8dd4a5dd52547a6728
J-Bunny560 <joshuamaldonado66@gmail.com>
2024-09-11 11:16:23 -0400

    fix

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,22 +1,14 @@
 #!/user/bin/python3
 def safe_print_list(my_list=[], x=0):
-    """
-    Prints x elements of a list and returns the real number of elements printed.
+    count = 0\
 
-    Args:
-        my_list (list): The list to print elements from. Defaults to an empty list.
-        x (int): The number of elements to print. Defaults to 0.
-
-    Returns:
-        int: The real number of elements printed.
-    """
-    try:
-        printed = 0
-        for i in range(x):
-            print(my_list[i], end='')
-            printed += 1
-        print()
-        return printed
-    except IndexError:
-        print()
-        return printed
+    if type(x) != int:
+        raise ValueError("is not and integer")
+    while count <x:
+        try:
+            print(my_list[count], end="")
+            count += 1
+        except IndexError:
+            break
+    print()
+    return count

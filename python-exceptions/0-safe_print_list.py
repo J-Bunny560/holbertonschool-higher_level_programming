5f1b581cafc5dc5639378bdcfcc7521525336814
J-Bunny560 <joshuamaldonado66@gmail.com>
2024-09-11 11:09:28 -0400

    fix

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -13,7 +13,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         printed = 0
         for i in range(x):
-            print(my_list[i], end=' ')
+            print(my_list[i], end='')
             printed += 1
         print()
         return printed

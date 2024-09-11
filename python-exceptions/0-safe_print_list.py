931e04706da4af4a98490efef50ecb4621247e35
J-Bunny560 <joshuamaldonado66@gmail.com>
2024-09-11 10:45:57 -0400

    fix

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -10,8 +10,8 @@ def safe_print_list(my_list=[], x=0):
     Returns:
         int: The real number of elements printed.
     """
+    count = 0
     try:
-        count = 0
         for i in range(x):
             print(my_list[i], end='')
             count += 1

7a69de5e0354a9c2d2cb6fb6e4a633fa6ed9fe7c
J-Bunny560 <joshuamaldonado66@gmail.com>
2024-09-12 13:54:18 -0400

    fix

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,8 +5,8 @@ def add_integer(a, b=98):
     This module adds two integers.
     It takes two arguments, a and b, and returns their sum.
     """
-    if type(a) not in [int, float]:
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     return int(a) + int(b)

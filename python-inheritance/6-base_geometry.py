7ed0ecbf8932872503ae4c7b9b07b260d9e252bc
J-Bunny560 <joshuamaldonado66@gmail.com>
2024-09-23 15:41:42 -0400

    improved geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines BaseGeometry class"""
+
+
+class BaseGeometry:
+    """Defines BaseGeometry class"""
+
+    def area(self):
+        """Not implemented"""
+        raise Exception("area() is not implemented")

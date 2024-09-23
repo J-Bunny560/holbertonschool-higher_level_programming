8b141677d8f129f052b3068bff2896c2c26bacb0
J-Bunny560 <joshuamaldonado66@gmail.com>
2024-09-23 13:19:45 -0400

    fix

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -6,4 +6,4 @@ inherited (directly or indirectly) from the specified class"""
 def inherits_from(obj, a_class):
     """fuction that return True if the objects is an instance of a class that
     inherited (directly or indirectly) from the specified class"""
-    return isinstance(obj, a_class) != a_class
+    return isinstance(obj, a_class) and type(obj) != a_class

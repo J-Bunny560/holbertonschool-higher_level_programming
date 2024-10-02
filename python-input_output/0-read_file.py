ffeb443d3712399ad213d236353fcd6c807bad42
J-Bunny560 <joshuamaldonado66@gmail.com>
2024-10-02 10:54:26 -0400

    fix

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,6 +3,7 @@
 
 
 def read_file(filename=""):
-    """write a function that reads a text file (UTF8) and prints it to stdout"""
+    """write a function that reads a text file (UTF8)
+        and prints it to stdout"""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")

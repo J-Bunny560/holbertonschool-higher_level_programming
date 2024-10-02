2a68aeb57a07ed858e926f558f38168d7b518ff2
J-Bunny560 <joshuamaldonado66@gmail.com>
2024-10-02 14:17:08 -0400

    task 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" a class Student that defines a student """
+
+
+class Student:
+    """ a class Student that defines a student """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ retrieves a dictionary representation of a Student instance """
+        if attrs is None:
+            return self.__dict__
+        else:
+            new_dict = {}
+            for i in attrs:
+                if i in self.__dict__:
+                    new_dict[i] = self.__dict__[i]
+            return new_dict

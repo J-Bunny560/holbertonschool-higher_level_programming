416f66edbd0d793dea213437eca53d5c64d9f584
J-Bunny560 <joshuamaldonado66@gmail.com>
2024-10-31 23:21:25 -0400

    fix

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-"""Script that prints all City objects from the database hbtn_0e_14_usa"""
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
-from model_city import City
+"""Lists states"""
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
-                        (sys.argv[1], sys.argv[2], sys.argv[3]),
-                        pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    rows = session.query(City, State).filter(City.state_id == State.id)\
-        .order_by(City.id).all()
-    for city, state in rows:
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
-    session.close()
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """Class representing the states table"""
+    __tablename__ = 'cities'
+
+    id = Column(Integer, nullable=False, primary_key=True,
+                autoincrement=True, unique=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)

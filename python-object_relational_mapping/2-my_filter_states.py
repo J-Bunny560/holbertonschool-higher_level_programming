a30f56daf2f560c3a117db045536f473a16d1eb7
J-Bunny560 <joshuamaldonado66@gmail.com>
2024-10-31 21:54:49 -0400

    fix

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,56 +1,31 @@
 #!/usr/bin/python3
-""" A script that utilizes MySQLdb package to list states in a database."""
+"""
+This script takes in an argument and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument.
+"""
+
 import MySQLdb
 
-def get_states(username, password, database, state_name):
-    """
-    Retrieves and displays all values in the states table of hbtn_0e_0_usa 
-    where name matches the given state name.
+if __name__ == "__main__":
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
 
-    Args:
-        username (str): MySQL username
-        password (str): MySQL password
-        database (str): Database name
-        state_name (str): State name to search for
-
-    Returns:
-        None
-    """
-
-    # Establish a connection to the MySQL server
-    conn = MySQLdb.connect(
+    db = MySQLdb.connect(
         host='localhost',
         port=3306,
-        user=username,
-        passwd=password,
-        db=database
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name
     )
 
-    # Create a cursor object to execute SQL queries
-    cur = conn.cursor()
-
-    # Create the SQL query with the user input
-    query = """
-        SELECT *
-        FROM states
-        WHERE name = %s
-        ORDER BY id ASC
-    """
-
-    # Execute the SQL query with the state name as a parameter
-    cur.execute(query, (state_name,))
-
-    # Fetch all the rows from the query result
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", (state_name,))
     rows = cur.fetchall()
 
-    # Display the results
     for row in rows:
         print(row)
 
-    # Close the cursor and connection
     cur.close()
-    conn.close()
-
-if __name__ == "__main__":
-    # Example usage:
-    get_states('username', 'password', 'hbtn_0e_0_usa', 'California')
+    db.close()
